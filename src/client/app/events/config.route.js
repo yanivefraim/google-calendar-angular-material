83171f08b5b067e745b5a31425861e72d0cde544
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-03-05 22:15:11 +0200

    rename folder

---
+++ src/client/app/events/config.route.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('calendar.events')
+        .run(appRun);
+
+    // appRun.$inject = ['routehelper']
+
+    /* @ngInject */
+    function appRun(routehelper) {
+        routehelper.configureRoutes(getRoutes());
+    }
+
+    function getRoutes() {
+        return [
+            {
+                url: '/events',
+                config: {
+                    templateUrl: 'app/events/events.tpl.html',
+                    controller: 'Avengers',
+                    controllerAs: 'vm',
+                    title: 'events',
+                    // settings: {
+                    //     nav: 2,
+                    //     content: '<i class="fa fa-lock"></i> Avengers'
+                    // }
+                }
+            }
+        ];
+    }
+})();
